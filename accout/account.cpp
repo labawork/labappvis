d86e2ed7ade2b3ade610c49f40bdd09521f3c113
OlyaOstrovskaya <56983605+OlyaOstrovskaya@users.noreply.github.com>
2020-11-13 23:38:00 +0300

    Update account.cpp

--- accout/account.cpp
+++ accout/account.cpp
@@ -1,42 +1,38 @@
 #include "account.h"
 #include<iostream>
-#include"user.h"
-#include"user.cpp"
 #include<fstream>
 
 
 using namespace std;
-void enter()
+
+void account::enter()
 {
 
-	account person1;
+	user person1;
 
-	cout << "write your login" << endl;
-		cin >> person1.numberuser;
-		
-	
+	cout << "Write your login" << endl;
+	cin >> person1.numberuser;
+
+	cout << "Write your password" << endl;
+
+	cin >> person1.password;
+	cout << "Put money" << endl;
+	cin >> person1.data;
+
+
 	cout << endl;
-	cout << "write ypur password" << endl;
-	for (int i = 0; i < 5; i++)
-	{
-		cin >> person1.password[i];
-		
-	}
-	cout << endl;
-	void createskey();
 	ofstream fin;
-	fin.open("person.txt", ofstream::app|ofstream::binary);
+	fin.open("person.txt", ofstream::app | ofstream::binary);
 	if (!fin.is_open())
 	{
 		cout << "error" << endl;
 	}
 	else
 	{
-		fin.write((char*)&person1, sizeof(account));
-	}	
+		fin.write((char*)&person1, sizeof(user));
+	}
 	fin.close();
-	
-	
+
 }
 
 
