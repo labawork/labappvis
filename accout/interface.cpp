063b043978ca83cb7b4c2cd458f2b8f20597eef5
OlyaOstrovskaya <56983605+OlyaOstrovskaya@users.noreply.github.com>
2020-11-13 23:31:41 +0300

    Update interface.cpp

--- accout/interface.cpp
+++ accout/interface.cpp
@@ -1,5 +1,31 @@
 #include"interface.h"
 #include<iostream>
+#include<fstream>
 
 using namespace std;
-//тут просто красивый вывод этого всего на экран
+
+void interface::show()
+{
+	user person1;
+	cout << "All information (admin access only)" << endl;
+	ifstream fin;
+	fin.open("person.txt", ofstream::app | ofstream::binary);
+	if (!fin.is_open())
+	{
+		cout << "error" << endl;
+	}
+	else
+	{
+		while (fin.read((char*)&person1, sizeof(user)))
+		{
+			cout << "User Number : " << person1.numberuser << "  ";
+			cout << "User Password : " << person1.password << " ";
+			cout << "User depozit : " << person1.data << " ";
+			cout << endl;
+
+		}
+
+	}
+	fin.close();
+
+}
