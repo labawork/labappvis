76c39dd9a0275085b0790544bf12896284d59b19
OlyaOstrovskaya <56983605+OlyaOstrovskaya@users.noreply.github.com>
2020-11-13 23:23:10 +0300

    Update user.cpp

--- accout/user.cpp
+++ accout/user.cpp
@@ -1,8 +1,25 @@
 #include "user.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
-void database()
+void user::database()
 {
-	//опиши тут отображение клиентов 
+	user person;
+	ifstream fin;
+	int number;
+	fin.open("person.txt", ofstream::app | ofstream::binary);
+	if (!fin.is_open())
+	{
+		cout << "error" << endl;
+	}
+	else
+	{
+		while (fin.read((char*)&person, sizeof(user)))
+		{
+			cout << "User Number is " << person.numberuser << endl;
+		}
+
+	}
+	fin.close();
 }
 
