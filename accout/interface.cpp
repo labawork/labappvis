f2b633f5dcb8ed674d3c2525750f5bc89ead3cc0
OlyaOstrovskaya <56983605+OlyaOstrovskaya@users.noreply.github.com>
2020-11-15 16:32:27 +0300

    Update interface.cpp

--- accout/interface.cpp
+++ accout/interface.cpp
@@ -9,7 +9,7 @@ void interface::show()
 	user person1;
 	cout << "All information (admin access only)" << endl;
 	ifstream fin;
-        fin.exceptions(ifstream::badbit | ifstream::failbit);
+        //fin.exceptions(ifstream::badbit | ifstream::failbit);
 	try
 	{
 		fin.open("person.txt", ofstream::app | ofstream::binary);
@@ -26,13 +26,21 @@ void interface::show()
 	}
 	else
 	{
-		while (fin.read((char*)&person1, sizeof(user)))
+		try
 		{
-			cout << "User Number : " << person1.numberuser << "  ";
-			cout << "User Password : " << person1.password << " ";
-			cout << "User depozit : " << person1.data << " ";
-			cout << endl;
+			while (fin.read((char*)&person1, sizeof(user)))
+			{
+				cout << "User Number : " << person1.numberuser << "  ";
+				cout << "User Password : " << person1.password << " ";
+				cout << "User depozit : " << person1.data << " ";
+				cout << endl;
 
+			}
+		}
+		catch (const std::exception&ex)
+		{
+			cout << ex.what() << endl;
+			cout << "Error" << endl;
 		}
 
 	}
